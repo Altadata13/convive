009fa94efb3a9ec76d8cac8dffe25fb229d47e25
Altadata <75349323+Altadata13@users.noreply.github.com>
2021-05-11 16:54:55 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,27 +81,3 @@ $(function() {
     });
     
 });
-<!-- Messenger plugin del chat Code -->
-    <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v10.0'
-          });
-        };
-
-        (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-      </script>
-
-      <!-- Your plugin del chat code -->
-      <div class="fb-customerchat"
-        attribution="biz_inbox"
-        page_id="693865687654838">
-      </div>
