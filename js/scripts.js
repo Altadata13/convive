020dc97a92dec50b6ab348e861c991e86876ba37
Altadata <75349323+Altadata13@users.noreply.github.com>
2021-05-16 17:28:19 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 (function($) {
     "use strict"; 
 	
@@ -14,7 +15,8 @@
 	});
 
 	
-	
+	/* Navbar Scripts */
+	// jQuery to collapse the navbar on scroll
     $(window).on('scroll load', function() {
 		if ($(".navbar").offset().top > 20) {
 			$(".fixed-top").addClass("top-nav-collapse");
@@ -23,6 +25,7 @@
 		}
     });
 
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
 		$(document).on('click', 'a.page-scroll', function(event) {
 			var $anchor = $(this);
@@ -33,21 +36,28 @@
 		});
 	});
 
+    // closes the responsive menu on menu item click
     $(".navbar-nav li a").on("click", function(event) {
     if (!$(this).parent().hasClass('dropdown'))
         $(".navbar-collapse").collapse('hide');
     });
 
 
+    /* Rotating Text - Morphtext */
 	$("#js-rotating").Morphext({
+		// The [in] animation type. Refer to Animate.css for a list of available animations.
 		animation: "fadeIn",
+		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
 		separator: ",",
+		// The delay between the changing of each phrase in milliseconds.
 		speed: 2000,
 		complete: function () {
+			// Called after the entrance animation is executed.
 		}
     });
     
 
+    /* Card Slider - Swiper */
 	var cardSlider = new Swiper('.card-slider', {
 		autoplay: {
             delay: 4000,
@@ -61,9 +71,11 @@
 		slidesPerView: 3,
 		spaceBetween: 20,
         breakpoints: {
+            // when window is <= 992px
             992: {
                 slidesPerView: 2
             },
+            // when window is <= 768px
             768: {
                 slidesPerView: 1
             } 
@@ -71,6 +83,7 @@
     });
 
     
+    /* Image Slider - Swiper */
     var imageSlider = new Swiper('.image-slider', {
         autoplay: {
             delay: 2000,
@@ -84,22 +97,27 @@
         spaceBetween: 30,
         slidesPerView: 5,
 		breakpoints: {
+            // when window is <= 380px
             380: {
                 slidesPerView: 1,
                 spaceBetween: 10
             },
+            // when window is <= 516px
             516: {
                 slidesPerView: 2,
                 spaceBetween: 10
             },
+            // when window is <= 768px
             768: {
                 slidesPerView: 3,
                 spaceBetween: 20
             },
+            // when window is <= 992px
             992: {
                 slidesPerView: 4,
                 spaceBetween: 30
             },
+            // when window is <= 1200px
             1200: {
                 slidesPerView: 5,
                 spaceBetween: 30
@@ -108,6 +126,7 @@
     });
 
 
+    /* Image Slider - Magnific Popup */
 	$('.popup-link').magnificPopup({
 		removalDelay: 300,
 		type: 'image',
@@ -120,11 +139,12 @@
 			}
 		},
 		gallery:{
-			enabled:true 
+			enabled:true //enable gallery mode
 		}
     });
 
     
+    /* Video Lightbox - Magnific Popup */
     $('.popup-youtube, .popup-vimeo').magnificPopup({
         disableOn: 700,
         type: 'iframe',
@@ -157,9 +177,10 @@
     });
 
 
+    /* Lightbox - Magnific Popup */
 	$('.popup-with-move-anim').magnificPopup({
 		type: 'inline',
-		fixedContentPos: false, 
+		fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
 		fixedBgPos: true,
 		overflowY: 'auto',
 		closeBtnInside: true,
@@ -170,9 +191,10 @@
 	});
     
 
+    /* Counter - CountTo */
 	var a = 0;
 	$(window).scroll(function() {
-		if ($('#counter').length) { 	
+		if ($('#counter').length) { // checking if CountTo section exists in the page, if not it will not run the script and avoid errors	
 			var oTop = $('#counter').offset().top - window.innerHeight;
 			if (a == 0 && $(window).scrollTop() > oTop) {
 			$('.counter-value').each(function() {
@@ -191,7 +213,7 @@
 					},
 					complete: function() {
 					$this.text(this.countNum);
-					
+					//alert('finished');
 					}
 				});
 			});
@@ -201,6 +223,8 @@
     });
 
 
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
     $("input, textarea").keyup(function(){
 		if ($(this).val() != '') {
 			$(this).addClass('notEmpty');
@@ -210,17 +234,21 @@
     });
 
 
+    /* Contact Form */
     $("#contactForm").validator().on("submit", function(event) {
     	if (event.isDefaultPrevented()) {
+            // handle the invalid form...
             cformError();
             csubmitMSG(false, "Please fill all fields!");
         } else {
+            // everything looks good!
             event.preventDefault();
             csubmitForm();
         }
     });
 
     function csubmitForm() {
+        // initiate variables with form content
 		var name = $("#cname").val();
 		var email = $("#cemail").val();
         var message = $("#cmessage").val();
@@ -243,8 +271,8 @@
     function cformSuccess() {
         $("#contactForm")[0].reset();
         csubmitMSG(true, "Message Submitted!");
-        $("input").removeClass('notEmpty');
-        $("textarea").removeClass('notEmpty'); 
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+        $("textarea").removeClass('notEmpty'); // resets the field label after submission
     }
 
     function cformError() {
@@ -263,17 +291,21 @@
     }
 
 
+    /* Privacy Form */
     $("#privacyForm").validator().on("submit", function(event) {
     	if (event.isDefaultPrevented()) {
+            // handle the invalid form...
             pformError();
             psubmitMSG(false, "Please fill all fields!");
         } else {
+            // everything looks good!
             event.preventDefault();
             psubmitForm();
         }
     });
 
     function psubmitForm() {
+        // initiate variables with form content
 		var name = $("#pname").val();
 		var email = $("#pemail").val();
         var select = $("#pselect").val();
@@ -297,7 +329,7 @@
     function pformSuccess() {
         $("#privacyForm")[0].reset();
         psubmitMSG(true, "Request Submitted!");
-        $("input").removeClass('notEmpty'); 
+        $("input").removeClass('notEmpty'); // resets the field label after submission
     }
 
     function pformError() {
@@ -316,7 +348,8 @@
     }
     
 
-   
+    /* Back To Top Button */
+    // create the back to top button
     $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
     var amountScrolled = 700;
     $(window).scroll(function() {
@@ -328,6 +361,7 @@
     });
 
 
+	/* Removes Long Focus On Buttons */
 	$(".button, a, button").mouseup(function() {
 		$(this).blur();
 	});
